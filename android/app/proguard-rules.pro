869f2864a86d006d11b614c46fa4170e93b160b7
NdalaTresorBuzangu <ndalabuzangu@gmail.com>
2026-04-20 10:07:57 +0000

    Add Android configuration

---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,2 @@
+# ProGuard / R8 rules for Document Seeker.
+# Minification is off by default; add keep rules here if you enable minify/shrink.
---
+++ android/app/src/main/kotlin/com/tshijuka/document_seeker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tshijuka.document_seeker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
